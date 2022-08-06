895dc8c337922907e310acbfce3801fe0d688591
dropdbassett <60153627+dropdbassett@users.noreply.github.com>
2022-08-06 01:53:11 +0100

    Verification pages
    Pulls userid from GET variables, and checks database for a corresponding user. Verifies if a match is found.

---
+++ webpages/index.php
@@ -0,0 +1,37 @@
+<html lang="en-UK">
+<head>
+	<title>Part III Verification</title>
+
+	<link rel="stylesheet" href="../css/2021jan23.css">
+</head>
+<body>
+<h1 style="text-align: center;">Part III Discord Verification</h1>
+<?php
+$id = $_GET['id'];
+if (ctype_digit($id)) { //Excludes empty string
+	if ($db = pg_connect("dbname=dra")) {
+		$query = "SELECT userid FROM partIII.members WHERE verifyd = '$id'";
+		$results = pg_query($db, $query);
+		
+		if ($user = pg_fetch_row($results)) {
+			$query = "UPDATE partIII.members SET verified = true, verifyd = '', manualverif = false WHERE userid = '$user[0]'";
+			pg_query($db, $query);
+			
+			echo "You have been successfully verified.";
+		}
+		else {
+			echo "No users found with verification ID $id; please check you have copied the link correctly.<br>If you just successfully verified and refreshed,";
+		}
+	}
+	else {
+		echo "Failed to connect to verification database. Please try again later.";
+	}
+	
+}
+else {
+	echo "Invalid user ID format; please check you have copied the link correctly.";
+}
+?>
+
+</body>
+</html>
